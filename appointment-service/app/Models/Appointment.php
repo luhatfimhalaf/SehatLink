14935799ea5d1a40099ed0ddccecf555b7d33624
luhatfimhalaf <luhatfim.halaf1@gmail.com>
2025-04-22 11:26:06 +0700

    update

--- appointment-service/app/Models/Appointment.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Model;
-
-class Appointment extends Model
-{
-    protected $fillable = ['patient_id', 'doctor_name', 'appointment_date', 'status', 'notes'];
-}
--- appointment-service/routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AppointmentController;
-
-Route::apiResource('appointments', AppointmentController::class);
